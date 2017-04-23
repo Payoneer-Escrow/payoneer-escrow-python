a4b6782c8becb93046dec498b2695f65728de993
Adam J Hartz <adam.hartz@armorpayments.com>
2017-04-23 17:21:09 -0400

    Initial work on Api and Authenticator

---
+++ payoneer_escrow_sdk/api.py
@@ -0,0 +1,38 @@
+from __future__ import absolute_import
+from authenticator import Authenticator
+
+
+class Api:
+
+    def __init__(self, api_key, api_secret, sandbox=False):
+        """
+        Initialize an API client.
+
+        Args:
+            api_key (string): Your API key
+            api_secret (string): Your API secret
+            sandbox (bool): Use the testing Sandbox environment (rather than
+                Production)?
+        """
+
+        self.authenticator = Authenticator(api_key, api_secret)
+        self.sandbox = sandbox
+
+    def accounts(self):
+        """
+        Access to the accounts associated with your API key.
+        """
+
+        #  return Accounts(self._base_url(), self.authenticator)
+        pass
+
+    def _base_url(self):
+        """
+        Return the proper base url depending on the environment.
+        """
+
+        if self.sandbox:
+            subdomain = 'sandbox'
+        else:
+            subdomain = 'app'
+        return 'https://' + subdomain + '/armorpayments.com'
---
+++ payoneer_escrow_sdk/authenticator.py
@@ -0,0 +1,66 @@
+from __future__ import absolute_import
+from datetime import datetime
+from hashlib import sha512
+
+
+class Authenticator:
+    """
+    Functionality used to authenticate all requests made to Payoneer Escrow.
+    """
+
+    def __init__(self, api_key, api_secret):
+        """
+        Initialize an Authenticator.
+
+        Args:
+            api_key (string): Your API key
+            api_secret (string): Your API secret
+        """
+
+        self.api_key = api_key
+        self.api_secret = api_secret
+
+    def secure_headers(self, method, uri):
+        """
+        Generate secure request authentication headers for Payoneer Escrow.
+
+        Payoneer Escrow used to be called Armor Payments, and the headers still
+        have that vestage.
+
+        Args:
+            method (string): The HTTP request method ('POST', 'GET', etc.)
+            uri (string): The request URI
+        """
+
+        timestamp = self._current_timestamp()
+        return dict({
+            'x-armorpayments-apikey': self.api_key,
+            'x-armorpayments-requesttimestamp': timestamp,
+            'x-armorpayments-signature': self._request_signature(
+                method, uri, timestamp),
+            })
+
+    def _current_timestamp(self):
+        """
+        Return a timestamp in the ISO 8601 format of 'YYYY-MM-DDTHH:MM:SS-HH:MM'
+        """
+
+        return datetime.utcnow().replace(microsecond=0).isoformat() + '-00:00'
+
+    def _request_signature(self, method, uri, timestamp):
+        """
+        Generate the unique request signature.
+
+        Args:
+            method (string): The HTTP request method ('POST', 'GET', etc.)
+            uri (string): The request URI
+            timestamp (string): The current time in ISO 8601 format
+        """
+
+        method = method.upper()
+        # For the authentication process, trim any params off of the URI
+        uri = uri.split('?')[0]
+        # Generate the signature
+        signature = "{}:{}:{}:{}".format(self.api_secret, method, uri, timestamp)
+        # Return the sha512 hash represented with only hexidecimal digits
+        return sha512(signature).hexdigest()
