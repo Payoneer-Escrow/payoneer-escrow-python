4adfd2d0db8a1b0009c1d691a79eb8d188d03ea4
Adam J Hartz <adam.hartz@armorpayments.com>
2017-04-22 23:41:56 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'An SDK for working with the Payoneer Escrow API',
+    'author': 'Adam J Hartz',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'adamha@payoneer.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['payoneer_escrow_sdk'],
+    'scripts': [],
+    'name': 'Payoneer Escrow SDK'
+}
+
+setup(**config)
