3b3a0304c2aebbc42e2dffbf1526ed649f6043cb
Adam J Hartz <adam.hartz@armorpayments.com>
2017-04-25 09:32:44 -0400

    Add `api_credentials.sample.py`, add .gitignore for `api_credentials.py`

---
+++ api_credentials.sample.py
@@ -0,0 +1,9 @@
+"""
+You can use this file as a template to store your api credentials. Just copy (or
+rename) this file to `api_credentials.py`. `api_credentials.py` is in this
+repository's `.gitignore` file.
+"""
+
+PAYONEER_ESCROW_KEY_TYPE = 'sandbox'
+PAYONEER_ESCROW_KEY = ''
+PAYONEER_ESCROW_SECRET = ''
