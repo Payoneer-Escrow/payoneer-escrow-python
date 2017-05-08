e15e01ae0998ac8be8aad910d6df6223996e2e55
Adam J Hartz <adam.hartz@armorpayments.com>
2017-05-08 10:06:48 -0400

    Update setup file with code from pip's setup.py
    * Add long_description, loaded in the same way as done by pip's setup.py
    (https://github.com/pypa/pip/blob/master/setup.py)
    * Arrange config parameters to match that of pip's and pipenv's setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,33 @@
+import codecs
+import os
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    # Intentionally *not* adding an encoding option to open
+    return codecs.open(os.path.join(here, *parts), 'r').read()
+
+
+long_description = read('README.md')
+
 config = {
-    'description': 'An SDK for working with the Payoneer Escrow API',
-    'author': 'Adam J Hartz',
-    'url': 'URL to get it at.',
-    'download_url': 'Where to download it.',
-    'author_email': 'adamha@payoneer.com',
+    'name': 'Payoneer Escrow SDK',
     'version': '0.1',
-    'install_requires': ['nose', 'requests'],
+    'description': 'An SDK for working with the Payoneer Escrow API',
+    'long_description': long_description,
+    'author': 'Adam J Hartz',
+    'author_email': 'adamha@payoneer.com',
+    'url': 'https://github.com/Payoneer-Escrow/payoneer-escrow-python',
     'packages': ['payoneer_escrow_sdk', 'payoneer_escrow_sdk.api'],
-    'scripts': [],
-    'name': 'Payoneer Escrow SDK'
-}
+    'install_requires': ['requests'],
+    'license': 'MIT',
+    'tests_require': ['nose'],
+    }
 
 setup(**config)
