2a33eb8877a0c9bbb337651e59ce587990d886f8
Adam J Hartz <adam.hartz@armorpayments.com>
2017-05-10 23:20:20 -0400

    Add classifiers, tighten up code with subpackage imports

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
-import codecs
-import os
+from codecs import open
+from os import path
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
 
-here = os.path.abspath(os.path.dirname(__file__))
+here = path.abspath(path.dirname(__file__))
 
 
 def read(*parts):
     # Intentionally *not* adding an encoding option to open
-    return codecs.open(os.path.join(here, *parts), 'r').read()
+    return open(path.join(here, *parts), 'r').read()
 
-
-long_description = read('README.md')
+long_description = read('README.rst')
 
 config = {
     'name': 'Payoneer Escrow SDK',
-    'version': '0.1',
+    'version': '0.1.0',
     'description': 'An SDK for working with the Payoneer Escrow API',
     'long_description': long_description,
     'author': 'Adam J Hartz',
@@ -27,7 +26,18 @@ config = {
     'packages': ['payoneer_escrow_sdk', 'payoneer_escrow_sdk.api'],
     'install_requires': ['requests'],
     'license': 'MIT',
-    'tests_require': ['nose'],
+    'classifiers': [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Software Development',
+        ],
+    'extras_require': {
+        'tests': ['nose'],
+        }
     }
 
 setup(**config)
